7c36dc00ba4face978341dd619c649ce1f18f625
willykane <willykane218@gmail.com>
2025-02-16 13:03:35 +0300

    Update code for volume of a cylinder.cpp

--- code for volume of a cylinder.cpp
+++ code for volume of a cylinder.cpp
@@ -8,6 +8,9 @@ REG NO: BSE-05-0044/2024 */
 #include <iostream>
 #include <cmath>
 using namespace std;
+
+/*function to calculate volume of the cylinder */
+
 double calculateCylinderVolume(double radius, double height) {
 
     double volume = M_PI * pow(radius, 2) * height;
@@ -15,12 +18,15 @@ double calculateCylinderVolume(double radius, double height) {
 }
 int main() {
     double radius, height;
-
+    
+//prompts the user to input radius & height of the cylinder //
+    
     cout << "Enter the radius of the cylinder: ";
     cin >> radius;
     cout << "Enter the height of the cylinder: ";
     cin >> height;
 
+    
     double volume = calculateCylinderVolume(radius, height);
     cout << "The volume of the cylinder is: " << volume << endl;
     return 0;
