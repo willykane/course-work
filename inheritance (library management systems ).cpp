53d4cb7210f208cfc997858d1eb3dd2e0c9cdac7
willykane <willykane218@gmail.com>
2025-03-02 19:12:32 +0300

    Add files via upload

---
+++ inheritance (library management systems ).cpp
@@ -0,0 +1,56 @@
+/*
+   AUTHOR:WILLYCE OJWANG
+   REG NO:BSE-05-0044/2024
+   DATE:2nd March 2025
+   a simple c++ program  demonstrating a library management system
+
+*/
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Person{
+protected :
+    string name;
+
+public :
+     void setName(string n) { name = n; }
+    string getName() { return name; }
+
+};
+
+class LibraryMember : public Person {
+private:
+    int memberID;
+    int booksBorrowed;
+public:
+    LibraryMember(string n, int id, int books) {
+        name = n;
+        memberID = id;
+        booksBorrowed = books;
+    }
+    int getMemberID() { return memberID; }
+    int getBooksBorrowed() { return booksBorrowed; }
+};
+
+class PremiumMember : public LibraryMember {
+private:
+    double membershipFee;
+public:
+    PremiumMember(string n, int id, int books, double fee)
+        : LibraryMember(n, id, books), membershipFee(fee) {}
+
+    double getMembershipFee() { return membershipFee; }
+};
+
+int main() {
+    PremiumMember pm("willyce", 101, 5, 50.0);
+
+    cout << "Name: " << pm.getName() << endl;
+    cout << "Member ID: " << pm.getMemberID() << endl;
+    cout << "Books Borrowed: " << pm.getBooksBorrowed() << endl;
+    cout << "Membership Fee: $" << pm.getMembershipFee() << endl;
+
+    return 0;
+}
